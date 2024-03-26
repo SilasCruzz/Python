8539d36a18ae82620d0fe7d86def2d356c158ce3
Silas Cruz <113648239+SilasCruzz@users.noreply.github.com>
2024-03-26 13:45:27 -0300

    Add files via upload

---
+++ Diferença.py
@@ -0,0 +1,10 @@
+print("=====Digite dois valores inteiros=====")
+num1= float(input("Digite valor um: "))
+num2= float (input("Digite valor dois: "))
+
+#calculo do sistema
+diferenca = num1 - num2
+resultado =  diferenca ** 2
+
+
+print(f"A diferença é {diferenca} e o resultado é {resultado}")
